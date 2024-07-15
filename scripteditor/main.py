2023b5e43ffa0c4659ad6ac51e7d735e4bbb0783
ada <git@adas.software>
2024-07-15 14:03:13 -0600

    okay made it not require cli or editing the script to choose the file

--- scripteditor/main.py
+++ scripteditor/main.py
@@ -71,28 +71,72 @@ class ScriptEditor:
             return False
 
 class ScriptEditorGUI(Gtk.Window):
-    def __init__(self, editor):
+    def __init__(self):
         Gtk.Window.__init__(self, title="Script Editor")
-        self.editor = editor
+        self.editor = None
 
         self.set_default_size(800, 600)
 
-        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
-        self.add(vbox)
+        main_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
+        self.add(main_box)
 
-        self.scene_liststore = Gtk.ListStore(str)
-        self.update_scene_liststore()
+        header = Gtk.HeaderBar()
+        header.set_show_close_button(True)
+        header.props.title = "Script Editor"
+        self.set_titlebar(header)
 
-        self.scene_combo = Gtk.ComboBox.new_with_model(self.scene_liststore)
-        renderer_text = Gtk.CellRendererText()
-        self.scene_combo.pack_start(renderer_text, True)
-        self.scene_combo.add_attribute(renderer_text, "text", 0)
-        vbox.pack_start(self.scene_combo, False, False, 0)
+        self.open_button = Gtk.Button.new_from_icon_name("document-open", Gtk.IconSize.BUTTON)
+        self.open_button.connect("clicked", self.on_open_clicked)
+        header.pack_start(self.open_button)
 
+        self.scene_combo = Gtk.ComboBoxText()
+        self.scene_combo.set_entry_text_column(0)
         self.scene_combo.connect("changed", self.on_scene_combo_changed)
+        header.pack_start(self.scene_combo)
+
+        self.add_scene_button = Gtk.Button.new_from_icon_name("list-add-symbolic", Gtk.IconSize.BUTTON)
+        self.add_scene_button.connect("clicked", self.on_add_scene_clicked)
+        header.pack_start(self.add_scene_button)
+
+        self.delete_scene_button = Gtk.Button.new_from_icon_name("list-remove-symbolic", Gtk.IconSize.BUTTON)
+        self.delete_scene_button.connect("clicked", self.on_delete_scene_clicked)
+        header.pack_start(self.delete_scene_button)
+
+        paned = Gtk.Paned(orientation=Gtk.Orientation.VERTICAL)
+        main_box.pack_start(paned, True, True, 0)
+
+        # Dialogue section
+        dialogue_frame = Gtk.Frame(label="Dialogue")
+        dialogue_frame.set_shadow_type(Gtk.ShadowType.ETCHED_IN)
+        paned.pack1(dialogue_frame, True, False)
+
+        dialogue_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+        dialogue_frame.add(dialogue_box)
 
         self.dialogue_textview = Gtk.TextView()
-        vbox.pack_start(self.dialogue_textview, True, True, 0)
+        self.dialogue_textview.set_wrap_mode(Gtk.WrapMode.WORD)
+        dialogue_scroll = Gtk.ScrolledWindow()
+        dialogue_scroll.add(self.dialogue_textview)
+        dialogue_box.pack_start(dialogue_scroll, True, True, 0)
+
+        dialogue_button_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=6)
+        dialogue_box.pack_start(dialogue_button_box, False, False, 0)
+
+        self.add_dialogue_button = Gtk.Button(label="Add Dialogue")
+        self.add_dialogue_button.connect("clicked", self.on_add_dialogue_clicked)
+        dialogue_button_box.pack_start(self.add_dialogue_button, True, True, 0)
+
+        self.delete_dialogue_button = Gtk.Button(label="Delete Dialogue")
+        self.delete_dialogue_button.connect("clicked", self.on_delete_dialogue_clicked)
+        dialogue_button_box.pack_start(self.delete_dialogue_button, True, True, 0)
+
+        # Choices section
+        choices_frame = Gtk.Frame(label="Choices")
+        choices_frame.set_shadow_type(Gtk.ShadowType.ETCHED_IN)
+        paned.pack2(choices_frame, True, False)
+
+        choices_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+        choices_frame.add(choices_box)
 
         self.choices_liststore = Gtk.ListStore(str, str)
         self.choices_treeview = Gtk.TreeView(model=self.choices_liststore)
@@ -100,44 +144,57 @@ class ScriptEditorGUI(Gtk.Window):
             renderer = Gtk.CellRendererText()
             column = Gtk.TreeViewColumn(column_title, renderer, text=i)
             self.choices_treeview.append_column(column)
-        vbox.pack_start(self.choices_treeview, True, True, 0)
+        choices_scroll = Gtk.ScrolledWindow()
+        choices_scroll.add(self.choices_treeview)
+        choices_box.pack_start(choices_scroll, True, True, 0)
 
-        button_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=6)
-        vbox.pack_start(button_box, False, False, 0)
-
-        self.add_scene_button = Gtk.Button(label="Add Scene")
-        self.add_scene_button.connect("clicked", self.on_add_scene_clicked)
-        button_box.pack_start(self.add_scene_button, True, True, 0)
-
-        self.delete_scene_button = Gtk.Button(label="Delete Scene")
-        self.delete_scene_button.connect("clicked", self.on_delete_scene_clicked)
-        button_box.pack_start(self.delete_scene_button, True, True, 0)
-
-        self.add_dialogue_button = Gtk.Button(label="Add Dialogue")
-        self.add_dialogue_button.connect("clicked", self.on_add_dialogue_clicked)
-        button_box.pack_start(self.add_dialogue_button, True, True, 0)
-
-        self.delete_dialogue_button = Gtk.Button(label="Delete Dialogue")
-        self.delete_dialogue_button.connect("clicked", self.on_delete_dialogue_clicked)
-        button_box.pack_start(self.delete_dialogue_button, True, True, 0)
+        choices_button_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=6)
+        choices_box.pack_start(choices_button_box, False, False, 0)
 
         self.add_choice_button = Gtk.Button(label="Add Choice")
         self.add_choice_button.connect("clicked", self.on_add_choice_clicked)
-        button_box.pack_start(self.add_choice_button, True, True, 0)
+        choices_button_box.pack_start(self.add_choice_button, True, True, 0)
 
         self.delete_choice_button = Gtk.Button(label="Delete Choice")
         self.delete_choice_button.connect("clicked", self.on_delete_choice_clicked)
-        button_box.pack_start(self.delete_choice_button, True, True, 0)
+        choices_button_box.pack_start(self.delete_choice_button, True, True, 0)
 
-    def update_scene_liststore(self):
-        self.scene_liststore.clear()
+    def on_open_clicked(self, button):
+        dialog = Gtk.FileChooserDialog(
+            title="Please choose a file", parent=self, action=Gtk.FileChooserAction.OPEN
+        )
+        dialog.add_buttons(
+            Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, Gtk.STOCK_OPEN, Gtk.ResponseType.OK
+        )
+
+        filter_json = Gtk.FileFilter()
+        filter_json.set_name("JSON files")
+        filter_json.add_mime_type("application/json")
+        dialog.add_filter(filter_json)
+
+        response = dialog.run()
+        if response == Gtk.ResponseType.OK:
+            file_path = dialog.get_filename()
+            self.editor = ScriptEditor(file_path)
+            self.update_scene_combo()
+            header = self.get_titlebar()
+            header.props.subtitle = file_path
+        
+        dialog.destroy()
+
+    def update_scene_combo(self):
+        active_scene = self.scene_combo.get_active_text()
+        self.scene_combo.remove_all()
         for scene in self.editor.list_scenes():
-            self.scene_liststore.append([scene])
+            self.scene_combo.append_text(scene)
+        if active_scene:
+            self.scene_combo.set_active_id(active_scene)
+        elif self.editor.list_scenes():
+            self.scene_combo.set_active(0)
 
     def on_scene_combo_changed(self, combo):
-        tree_iter = combo.get_active_iter()
-        if tree_iter is not None:
-            scene = combo.get_model()[tree_iter][0]
+        scene = combo.get_active_text()
+        if scene:
             self.load_scene(scene)
 
     def load_scene(self, scene):
@@ -157,35 +214,34 @@ class ScriptEditorGUI(Gtk.Window):
             buttons=Gtk.ButtonsType.OK_CANCEL,
             text="Enter new scene name:",
         )
-        dialog.set_default_size(150, 100)
+        dialog.set_default_size(300, 100)
         entry = Gtk.Entry()
         entry.set_text("new_scene")
-        dialog.get_content_area().pack_start(entry, False, False, 0)
+        dialog.get_content_area().pack_start(entry, True, True, 0)
         entry.show()
         response = dialog.run()
         if response == Gtk.ResponseType.OK:
             scene_name = entry.get_text()
             if self.editor.add_scene(scene_name):
-                self.update_scene_liststore()
+                self.update_scene_combo()
+                self.scene_combo.set_active_id(scene_name)
             else:
-                print(f"Scene '{scene_name}' already exists.")
+                self.show_error_dialog(f"Scene '{scene_name}' already exists.")
         dialog.destroy()
 
     def on_delete_scene_clicked(self, button):
-        tree_iter = self.scene_combo.get_active_iter()
-        if tree_iter is not None:
-            scene = self.scene_combo.get_model()[tree_iter][0]
+        scene = self.scene_combo.get_active_text()
+        if scene:
             if self.editor.delete_scene(scene):
-                self.update_scene_liststore()
+                self.update_scene_combo()
                 self.dialogue_textview.get_buffer().set_text("")
                 self.choices_liststore.clear()
             else:
-                print(f"Scene '{scene}' does not exist.")
+                self.show_error_dialog(f"Scene '{scene}' does not exist.")
 
     def on_add_dialogue_clicked(self, button):
-        tree_iter = self.scene_combo.get_active_iter()
-        if tree_iter is not None:
-            scene = self.scene_combo.get_model()[tree_iter][0]
+        scene = self.scene_combo.get_active_text()
+        if scene:
             dialog = Gtk.MessageDialog(
                 transient_for=self,
                 flags=0,
@@ -193,15 +249,14 @@ class ScriptEditorGUI(Gtk.Window):
                 buttons=Gtk.ButtonsType.OK_CANCEL,
                 text="Enter character and dialogue:",
             )
-            dialog.set_default_size(150, 100)
+            dialog.set_default_size(300, 150)
             character_entry = Gtk.Entry()
             character_entry.set_text("character")
             text_entry = Gtk.Entry()
             text_entry.set_text("dialogue")
-            dialog.get_content_area().pack_start(character_entry, False, False, 0)
-            dialog.get_content_area().pack_start(text_entry, False, False, 0)
-            character_entry.show()
-            text_entry.show()
+            dialog.get_content_area().pack_start(character_entry, True, True, 0)
+            dialog.get_content_area().pack_start(text_entry, True, True, 0)
+            dialog.show_all()
             response = dialog.run()
             if response == Gtk.ResponseType.OK:
                 character = character_entry.get_text()
@@ -209,13 +264,12 @@ class ScriptEditorGUI(Gtk.Window):
                 if self.editor.add_dialogue(scene, character, text):
                     self.load_scene(scene)
                 else:
-                    print(f"Failed to add dialogue to scene '{scene}'.")
+                    self.show_error_dialog(f"Failed to add dialogue to scene '{scene}'.")
             dialog.destroy()
 
     def on_delete_dialogue_clicked(self, button):
-        tree_iter = self.scene_combo.get_active_iter()
-        if tree_iter is not None:
-            scene = self.scene_combo.get_model()[tree_iter][0]
+        scene = self.scene_combo.get_active_text()
+        if scene:
             buffer = self.dialogue_textview.get_buffer()
             start, end = buffer.get_bounds()
             dialogue_lines = buffer.get_text(start, end, True).split("\n")
@@ -224,9 +278,8 @@ class ScriptEditorGUI(Gtk.Window):
                 self.load_scene(scene)
 
     def on_add_choice_clicked(self, button):
-        tree_iter = self.scene_combo.get_active_iter()
-        if tree_iter is not None:
-            scene = self.scene_combo.get_model()[tree_iter][0]
+        scene = self.scene_combo.get_active_text()
+        if scene:
             dialog = Gtk.MessageDialog(
                 transient_for=self,
                 flags=0,
@@ -234,15 +287,14 @@ class ScriptEditorGUI(Gtk.Window):
                 buttons=Gtk.ButtonsType.OK_CANCEL,
                 text="Enter choice text and next scene:",
             )
-            dialog.set_default_size(150, 100)
+            dialog.set_default_size(300, 150)
             text_entry = Gtk.Entry()
             text_entry.set_text("choice text")
             next_scene_entry = Gtk.Entry()
             next_scene_entry.set_text("next scene")
-            dialog.get_content_area().pack_start(text_entry, False, False, 0)
-            dialog.get_content_area().pack_start(next_scene_entry, False, False, 0)
-            text_entry.show()
-            next_scene_entry.show()
+            dialog.get_content_area().pack_start(text_entry, True, True, 0)
+            dialog.get_content_area().pack_start(next_scene_entry, True, True, 0)
+            dialog.show_all()
             response = dialog.run()
             if response == Gtk.ResponseType.OK:
                 text = text_entry.get_text()
@@ -250,13 +302,12 @@ class ScriptEditorGUI(Gtk.Window):
                 if self.editor.add_choice(scene, text, next_scene):
                     self.load_scene(scene)
                 else:
-                    print(f"Failed to add choice to scene '{scene}'.")
+                    self.show_error_dialog(f"Failed to add choice to scene '{scene}'.")
             dialog.destroy()
 
     def on_delete_choice_clicked(self, button):
-        tree_iter = self.scene_combo.get_active_iter()
-        if tree_iter is not None:
-            scene = self.scene_combo.get_model()[tree_iter][0]
+        scene = self.scene_combo.get_active_text()
+        if scene:
             selection = self.choices_treeview.get_selection()
             model, tree_iter = selection.get_selected()
             if tree_iter:
@@ -264,9 +315,18 @@ class ScriptEditorGUI(Gtk.Window):
                 self.editor.delete_choice(scene, choice_index)
                 self.load_scene(scene)
 
-# Usage example
-editor = ScriptEditor('./vn_script.json')
-win = ScriptEditorGUI(editor)
+    def show_error_dialog(self, message):
+        dialog = Gtk.MessageDialog(
+            transient_for=self,
+            flags=0,
+            message_type=Gtk.MessageType.ERROR,
+            buttons=Gtk.ButtonsType.OK,
+            text=message,
+        )
+        dialog.run()
+        dialog.destroy()
+
+win = ScriptEditorGUI()
 win.connect("destroy", Gtk.main_quit)
 win.show_all()
 Gtk.main()
